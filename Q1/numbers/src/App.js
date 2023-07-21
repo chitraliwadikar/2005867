20a4589089d2830f1dd1297a4a0a0403c4773ffa
chitraliwadikar <chitrali.wadikar@gmail.com>
2023-07-21 16:46:43 +0530

    Changes in App.js

--- Q1/numbers/src/App.js
+++ Q1/numbers/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [numbers, setNumbers] = useState([]);
+  const apiUrl = 'http://localhost:8008/numbers';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const params = new URLSearchParams();
+      params.append('url', 'http://20.244.56.144/numbers/primes');
+      params.append('url', 'http://20.244.56.144/numbers/fibo');
+      params.append('url', 'http://20.244.56.144/numbers/odd');
+
+      try {
+        const response = await axios.get(apiUrl, { params });
+        setNumbers(response.data.numbers);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Number Management Frontend</h1>
+      <ul>
+        {numbers.map((number, index) => (
+          <li key={index}>{number}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
